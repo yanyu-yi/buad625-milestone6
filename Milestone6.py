423320ef2596bf2604404784b24c88500cd776cf
yanyu-yi <104039806+yanyu-yi@users.noreply.github.com>
2022-04-21 23:30:15 -0400

    Add files via upload

--- Milestone6.py
+++ Milestone6.py
@@ -28,6 +28,35 @@ if urlinput != '':
     file_list.sort()
     # remove .jpg
     new_list = [s.replace(".jpg", "") for s in file_list]
+
+    # input csv
+    # read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
+    CustomerList = pd.read_csv("./liveCustomerList.csv")
+    CustomerList['firstName'] = CustomerList['firstName'].str.upper()
+    CustomerList['lastName'] = CustomerList['lastName'].str.upper()
+    FraudList = pd.read_csv("./liveFraudList.csv")
+    FraudList["fraudster"] = '1'
+
+    # change custID list to Dataframe
+    custID = pd.DataFrame(new_list, columns=['custID'])
+    # change datatype
+    custID['custID'] = custID['custID'].astype(int)
+
+    # left join table custID and customerlist
+    custIDname = pd.merge(custID, CustomerList, on='custID', how='left')
+    custIDnameFraud = pd.merge(custIDname, FraudList, on=[
+        'firstName', 'lastName'], how='left')
+    custIDnameFraud['fraudster'] = custIDnameFraud['fraudster'].fillna(0)
+
+    # FraudTestOnput.csv
+    output = custIDnameFraud[['custID', 'fraudster']]
+
+    @st.cache
+    def convert_df_to_csv(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv(index=False).encode('utf-8')
+
+
 else:
     st.text('Please enter url above and click the Submit button')
 
@@ -51,33 +80,33 @@ else:
 # # remove .jpg
 # new_list = [s.replace(".jpg", "") for s in file_list]
 
-# input csv
-# read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
-CustomerList = pd.read_csv("./liveCustomerList.csv")
-CustomerList['firstName'] = CustomerList['firstName'].str.upper()
-CustomerList['lastName'] = CustomerList['lastName'].str.upper()
-FraudList = pd.read_csv("./liveFraudList.csv")
-FraudList["fraudster"] = '1'
+# # input csv
+# # read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
+# CustomerList = pd.read_csv("./liveCustomerList.csv")
+# CustomerList['firstName'] = CustomerList['firstName'].str.upper()
+# CustomerList['lastName'] = CustomerList['lastName'].str.upper()
+# FraudList = pd.read_csv("./liveFraudList.csv")
+# FraudList["fraudster"] = '1'
 
-# change custID list to Dataframe
-custID = pd.DataFrame(new_list, columns=['custID'])
-# change datatype
-custID['custID'] = custID['custID'].astype(int)
+# # change custID list to Dataframe
+# custID = pd.DataFrame(new_list, columns=['custID'])
+# # change datatype
+# custID['custID'] = custID['custID'].astype(int)
 
-# left join table custID and customerlist
-custIDname = pd.merge(custID, CustomerList, on='custID', how='left')
-custIDnameFraud = pd.merge(custIDname, FraudList, on=[
-                           'firstName', 'lastName'], how='left')
-custIDnameFraud['fraudster'] = custIDnameFraud['fraudster'].fillna(0)
+# # left join table custID and customerlist
+# custIDname = pd.merge(custID, CustomerList, on='custID', how='left')
+# custIDnameFraud = pd.merge(custIDname, FraudList, on=[
+#                            'firstName', 'lastName'], how='left')
+# custIDnameFraud['fraudster'] = custIDnameFraud['fraudster'].fillna(0)
 
-# FraudTestOnput.csv
-output = custIDnameFraud[['custID', 'fraudster']]
+# # FraudTestOnput.csv
+# output = custIDnameFraud[['custID', 'fraudster']]
 
 
-@st.cache
-def convert_df_to_csv(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv(index=False).encode('utf-8')
+# @st.cache
+# def convert_df_to_csv(df):
+#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+#     return df.to_csv(index=False).encode('utf-8')
 
 
 if urlinput != '':
